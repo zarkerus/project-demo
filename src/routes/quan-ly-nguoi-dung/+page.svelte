5b6e45d6851a3e3bf3e9126885044aa9a76998cc
zarkerus <zarkerus@gmail.com>
2024-09-23 15:31:23 +0700

    code danh muc nguoi dung

---
+++ src/routes/quan-ly-nguoi-dung/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+    let users = [
+        {
+            ho_ten: "Nghiem Duong Hung",
+            gioi_tinh: "Nam"
+        },
+        {
+            ho_ten: "Nghiem Duong Hung 1",
+            gioi_tinh: "Nam"
+        },{
+            ho_ten: "Nghiem Duong Hung 2 ",
+            gioi_tinh: "Nam"
+        },
+        {
+            ho_ten: "Nghiem Duong Hung3",
+            gioi_tinh: "Nam"
+        }
+    ]
+</script>
+
+<table>
+    <thead>
+        <th>STT</th>
+        <th>Ho va Ten</th>
+        <th>Gioi Tinh</th>
+    </thead>
+    <tbody>
+        {#each users as user, index (index)}
+            <tr>
+                <td>index</td>
+                <td>{user.ho_ten}</td>
+                <td>{user.gioi_tinh}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
